819f0102c81b99e1a00aba6163053410cee2f3be
arielc19 <63265121+arielc19@users.noreply.github.com>
2020-11-17 13:33:38 -0500

    Create StateTest.java

---
+++ src/prj5/StateTest.java
@@ -0,0 +1,18 @@
+package prj5;
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- arielc19
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ * This class represents the individual states from the Covid-19 data. It stores
+ * the race information regarding to a specific state.
+ * 
+ * @author Ariel Carter
+ * @version 11/17/2020
+ */
