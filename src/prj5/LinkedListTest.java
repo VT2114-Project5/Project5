4725446fde956a23c8a0196980226960e228403e
arielc19 <63265121+arielc19@users.noreply.github.com>
2020-11-20 17:20:45 -0500

    Update and rename LinkedListTest to LinkedListTest.java

---
+++ src/prj5/LinkedListTest.java
@@ -0,0 +1,19 @@
+package prj5;
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- arielc19
+
+import java.util.Collections;
+import java.util.LinkedList;
+import student.TestCase
+
+/**
+ * This class tests all of the methods in the Race Class
+ * 
+ * @author Ariel Carter
+ * @version 11/17/2020
+ */
+
