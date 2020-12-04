064c8ba16215cf3952176d0e3b43f866f55066d7
Adam Oswald <3386201+amrios@users.noreply.github.com>
2020-12-03 21:39:46 -0500

    GUIControllerTest: Added tests for sort maintenance

--- src/prj5/GUIControllerTest.java
+++ src/prj5/GUIControllerTest.java
@@ -24,14 +24,29 @@ public class GUIControllerTest extends TestCase {
         controller = new GUIController(read.getStates());
     }
 
+    
+    public void testMaintainSort() {
+        controller.setCurrentState("VA");
+        controller.sortCfr();
+        assertTrue(controller.getRaceArray()[0].getName().equals("white"));
+        controller.setCurrentState("MD");
+        assertTrue(controller.getRaceArray()[0].getName().equals("asian"));
+        controller.setCurrentState("NC");
+        assertTrue(controller.getRaceArray()[0].getName().equals("black"));
+        controller.sortAlpha();
+        assertTrue(controller.getRaceArray()[0].getName().equals("asian"));
+        controller.setCurrentState("VA");
+        assertTrue(controller.getRaceArray()[0].getName().equals("asian"));
+    }
 
     public void testInitializeNull() {
         LinkedList<State> nullList = null;
-        GUIController testControl;
+        GUIController testControl = null;
         
         try {
             testControl = new GUIController(nullList);
             fail("Tried to setup a controller that uses a null object.");
+            testControl.getActiveState();
         }
         catch (IllegalArgumentException e) {
             // This catch block has been left blank.
